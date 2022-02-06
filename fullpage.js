6a57a1bed595f1423a99007950705c93d7972e0e
Hackerman <evenengesland@gmail.com>
2022-02-06 12:29:36 +0100

    Delete fullpage.js

--- fullpage.js
+++
@@ -1,8 +0,0 @@
-new fullpage('#fullpage', {
-    licenseKey: null,
-    autoScrolling: true,
-    navigation: true,
-    navigationPosition: 'right',
-    navigationTooltips:['Home','Karazhan','Gruul and Magtheridon','Serpentshrine Cavern','Tempest Keep', 'Mount Hyjal', 'Black Temple'],
-    showActiveTooltip: true,
-})
