3b6f4a398682d85031df40c3c69fa301abafe35b
Hackerman <evenengesland@gmail.com>
2022-02-06 09:35:28 +0100

    Add files via upload

---
+++ wow/raid-tool-old/fullpage.js
@@ -0,0 +1,8 @@
+new fullpage('#fullpage', {
+    licenseKey: null,
+    autoScrolling: true,
+    navigation: true,
+    navigationPosition: 'right',
+    navigationTooltips:['Home','Karazhan','Gruul and Magtheridon','Serpentshrine Cavern','Tempest Keep', 'Mount Hyjal', 'Black Temple'],
+    showActiveTooltip: true,
+})
