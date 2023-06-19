d2f646761f1d79e5ae55b75f9e22606bbf94d1c8
Evvy <evenengesland@gmail.com>
2023-06-19 18:35:14 +0200

    Update main.js

--- test/js/main.js
+++ test/js/main.js
@@ -1,30 +1,55 @@
 // Fetch JSON data
-fetch('json/permissions.json')
+fetch('data/roles.json')
   .then(response => response.json())
   .then(data => {
     const permissionsGrid = document.getElementById('permissions-grid');
 
-    // Iterate over roles
-    data.roles.forEach(role => {
-      const roleContainer = document.createElement('div');
-      roleContainer.className = 'role-container';
-      roleContainer.innerHTML = `<h3>${role.name}</h3>`;
+    // Function to update the permissions grid
+    const updateGrid = () => {
+      // Clear the permissions grid
+      permissionsGrid.innerHTML = '';
 
-      // Iterate over permissions
-      role.permissions.forEach(permission => {
-        const permissionItem = document.createElement('div');
-        permissionItem.className = 'permission-item';
-        const inputId = `${role.name}-${permission.name.replace(/\s+/g, '-')}`;
+      // Iterate over roles
+      data.roles.forEach(role => {
+        const roleContainer = document.createElement('div');
+        roleContainer.className = 'role-container';
+        roleContainer.innerHTML = `<h3>${role.name}</h3>`;
 
-        // Create checkbox for each permission
-        permissionItem.innerHTML = `
-          <input type="checkbox" id="${inputId}" ${permission.enabled ? 'checked' : ''}>
-          <label for="${inputId}">${permission.name}</label>
-        `;
+        // Iterate over permissions
+        role.permissions.forEach(permission => {
+          const permissionItem = document.createElement('div');
+          permissionItem.className = 'permission-item';
+          const permissionSymbol = permission.enabled ? '✅' : '❌';
 
-        roleContainer.appendChild(permissionItem);
+          // Create symbol for each permission
+          permissionItem.innerHTML = `
+            <span>${permissionSymbol}</span>
+            <label>${permission.name}</label>
+          `;
+
+          roleContainer.appendChild(permissionItem);
+        });
+
+        permissionsGrid.appendChild(roleContainer);
       });
+    };
 
-      permissionsGrid.appendChild(roleContainer);
-    });
-});
+    // Initial grid update
+    updateGrid();
+
+    // Function to toggle permission status
+    const togglePermission = (roleIndex, permissionIndex) => {
+      data.roles[roleIndex].permissions[permissionIndex].enabled = !data.roles[roleIndex].permissions[permissionIndex].enabled;
+      updateGrid();
+    };
+
+    // Add event listeners for permission toggles
+    const permissionItems = document.getElementsByClassName('permission-item');
+    for (let i = 0; i < permissionItems.length; i++) {
+      permissionItems[i].addEventListener('click', function () {
+        const roleIndex = Math.floor(i / role.permissions.length);
+        const permissionIndex = i % role.permissions.length;
+        togglePermission(roleIndex, permissionIndex);
+      });
+    }
+  });
