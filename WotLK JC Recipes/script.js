580c7e1a06998c327696e51242c1fff088bba927
Evvy <evenengesland@gmail.com>
2023-06-21 06:33:10 +0200

    Update script.js

--- WotLK JC Recipes/script.js
+++ WotLK JC Recipes/script.js
@@ -21,9 +21,7 @@ fetch('gems.json')
             <p>${gem.stats}</p>
             <p class="currency">Cost: 4<img src="https://wow.zamimg.com/images/wow/icons/tiny/inv_misc_gem_variety_01.gif" alt="inv_misc_gem_variety_01"></p>
             <p>Members:${gem.members.length}</p>
-            ${gem.members.forEach(member => 
-              console.log("<p>Name:" + member.name + "</p>")
-            )}
+            <p>${gem.members.forEach(member => member.name)}</p>
         </div>
         `;
 
